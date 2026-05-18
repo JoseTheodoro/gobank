c4c8ba3ead50433ddfd1edd631052feba9d0d4cc
Jose Theodoro <jose.theodoro42@gmail.com>
2026-05-18 16:19:06 -0300

    adding first version grpc servers

---
+++ ms-kyc/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "gobank/contracts/pb/kyc"
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedKYCServer
+}
+
+func (s *Server) StartKYC(ctx context.Context, in *pb.StartKYCRequest) (*pb.StartKYCResponse, error) {
+	return &pb.StartKYCResponse{
+		B: fmt.Sprintf("%s, hello from ms-kyc", in.GetName()),
+	}, nil
+}
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error on load .env file")
+	}
+
+	conn, err := net.Listen("tcp", os.Getenv("ADDR_LISTEN"))
+	if err != nil {
+		log.Fatalf("error on connect to tcp: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterKYCServer(s, &Server{})
+	fmt.Printf("gRPC server started at localhost%s\n", os.Getenv("ADDR_LISTEN"))
+	if err := s.Serve(conn); err != nil {
+		log.Fatalf("error on grpc server: %v", err)
+	}
+
+}
